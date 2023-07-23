a2793a5a463e8a910147c3b69fd31e1d94a399a9
psg9790 <psg9790@gmail.com>
2023-07-23 16:12:43 +0900

    230723

--- Personal/psg9790/baekjoon/9466.cpp
+++ Personal/psg9790/baekjoon/9466.cpp
@@ -6,7 +6,7 @@ void dfs(int node, vector<int> &lines, const vector<int> &arr, vector<bool> &vis
     lines.push_back(node);
     visited[node] = true;
 
-    if (globalVisited[arr[node]])
+    if (globalVisited[arr[node]]) // already visited, ignore previous members
     {
         for (vector<int>::iterator iter2 = --lines.end(); iter2 != --lines.begin(); iter2--)
         {
@@ -14,25 +14,25 @@ void dfs(int node, vector<int> &lines, const vector<int> &arr, vector<bool> &vis
         }
         return;
     }
-    if (visited[arr[node]])
+
+    if (visited[arr[node]]) //  cycle found
     {
-        for (vector<int>::iterator iter = --lines.end(); iter != --lines.begin(); iter--)
+        for (vector<int>::iterator iter = --lines.end(); iter != --lines.begin(); iter--) // add cycle members in result
         {
             result.insert((*iter));
-            // cout << " *** " << (*iter) << '\n';
             globalVisited[(*iter)] = true;
             if ((*iter) == arr[node])
             {
-                for (vector<int>::iterator iter2 = next(iter, -1); iter2 != --lines.begin(); iter2--)
+                for (vector<int>::iterator iter2 = next(iter, -1); iter2 != --lines.begin(); iter2--) // ignore non-cycle members
                 {
                     globalVisited[(*iter2)] = true;
                 }
-                break;
+                break; // cycle ends
             }
         }
         return;
     }
-    dfs(arr[node], lines, arr, visited, globalVisited, result);
+    dfs(arr[node], lines, arr, visited, globalVisited, result); // continue dfs
 }
 
 void solve()
@@ -48,15 +48,15 @@ void solve()
     set<int> result;
     for (int i = 1; i <= n; i++)
     {
-        if (!globalVisited[i])
+        if (!globalVisited[i]) // dfs start
         {
-
             vector<bool> visited(n + 1, false);
             vector<int> lines;
             dfs(i, lines, arr, visited, globalVisited, result);
         }
     }
 
+    // total - cycle
     cout << n - result.size() << '\n';
 }
 
