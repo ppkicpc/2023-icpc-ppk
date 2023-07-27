4c0dc518078257650874bb9136d4eeaea213d884
psg9790 <psg9790@gmail.com>
2023-07-27 23:18:05 +0900

    230727

---
+++ CP/CodeForces/230727 Educational Codeforces Round 152 (Rated for Div. 2)/psg9790/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
